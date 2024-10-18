2b13940a22e5a7debe3ae3540982add6b4677411
developerabrorbek <abdulkhamidovabrorbek@gmail.com>
2024-10-18 16:36:32 +0500

    chat update handle messages add

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CategoryModule, ChatModule, ProductModule } from '@modules';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [CategoryModule, ProductModule, ChatModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'client'),
+      serveRoot: '/static',
+    }),
+    CategoryModule,
+    ProductModule,
+    ChatModule,
+  ],
 })
 export class AppModule {}
--- gateway/src/modules/chat/chat.gateway.ts
+++ gateway/src/modules/chat/chat.gateway.ts
@@ -7,6 +7,28 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
+enum MessageType {
+  message = 'message',
+  joined = 'joined',
+}
+
+interface UserMessageType {
+  id: number;
+  message: string;
+  type: MessageType;
+  createdAt: string;
+  userId: number;
+  user?: User;
+}
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const users: User[] = [];
+const messages: UserMessageType[] = [];
+
 @WebSocketGateway({
   cors: {
     origin: '*',
@@ -16,13 +38,48 @@ export class ChatGateway {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('typing')
-  handleTyping(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
-    client.broadcast.emit('typing', {user: data.user});
+  @SubscribeMessage('messages')
+  handleMessage(@MessageBody() data: any) {
+    const newMessage: UserMessageType = {
+      id: messages.at(-1)?.id + 1 || 1,
+      createdAt: new Date().toTimeString().slice(0, 8),
+      message: data.message,
+      type: MessageType.message,
+      userId: data.userId,
+    };
+    const foundedUser = users.find((u) => u.id == data.userId);
+    newMessage.user = foundedUser;
+    messages.push(newMessage);
+
+    this.server.emit('messages', messages);
   }
 
-  @SubscribeMessage("joined")
+  @SubscribeMessage('typing')
+  handleTyping(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
+    client.broadcast.emit('typing', { user: data.user });
+  }
+
+  @SubscribeMessage('joined')
   handleJoin(@MessageBody() data: any) {
-    this.server.emit('joined', { user: data.user});
+    const newUser: User = { id: users.at(-1)?.id + 1 || 1, name: data.user };
+    users.push(newUser);
+
+    const newMessage: UserMessageType = {
+      id: messages.at(-1)?.id + 1 || 1,
+      createdAt: new Date().toTimeString().slice(0, 8),
+      message: `${newUser.name} joined the chat`,
+      type: MessageType.joined,
+      userId: newUser.id,
+    };
+
+    messages.push(newMessage);
+
+    messages.forEach((m) => {
+      const foundedUser = users.find((u) => u.id == m.userId);
+      m.user = foundedUser;
+    });
+
+    this.server.emit('messages', messages);
+    this.server.emit('joined', { user: data.user, id: newUser.id });
   }
 }
